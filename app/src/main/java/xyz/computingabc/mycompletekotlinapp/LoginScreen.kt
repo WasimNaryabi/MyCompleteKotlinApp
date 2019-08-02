9d04dff1183d89b895c21335211528f7b7fea7d8
Wasim Ahmad <Waseemnaryabi2018@gmail.com>
2019-08-02 16:00:59 +0500

    loginDesign commet

--- app/src/main/java/xyz/computingabc/mycompletekotlinapp/LoginScreen.kt
+++ app/src/main/java/xyz/computingabc/mycompletekotlinapp/LoginScreen.kt
@@ -3,12 +3,50 @@ package xyz.computingabc.mycompletekotlinapp
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_login_screen.*
 
 class LoginScreen : AppCompatActivity() {
+
+   private var emailString:String? = null
+   private var passwordString:String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
+
+
+        btn_login.setOnClickListener(View.OnClickListener {
+            validation()
+            val snack = Snackbar.make(root_layout,"Invalid Email Or Password",Snackbar.LENGTH_LONG)
+            snack.show()
+        })
+
+    }
+
+    private fun validation() {
+        emailString = input_email.text.toString().trim()
+        passwordString = input_password?.text.toString().trim()
+
+        if(emailString.isNullOrEmpty()){
+            input_email.error="plz enter your Email"
+        }else if(passwordString.isNullOrEmpty()){
+            input_password.error="plz enter your Password"
+        }else{
+            if(!(emailString.equals("wasim11@gmail.com") && passwordString.equals("12345678"))){
+                val snack = Snackbar.make(root_layout,"Invalid Email Or Password",Snackbar.LENGTH_LONG)
+                snack.show()
+            }else{
+                val home = Intent(applicationContext,Home::class.java)
+                startActivity(home)
+                finish()
+            }
+        }
     }
 
     override fun onBackPressed() {
@@ -17,4 +55,6 @@ class LoginScreen : AppCompatActivity() {
         startActivity(home)
         finish()
     }
+
+
 }
